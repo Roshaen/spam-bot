4b7adb8823b4b5a0be01d638cfbff49cda19b8e2
Roshan Kumar <58213083+Roshaen@users.noreply.github.com>
2020-09-08 23:36:14 +0530

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,27 @@
+import pyautogui as pg
+from time import sleep
+import sys
+sleep(3)
+
+class Spam:
+    def __init__(self, word, n):
+        self.word = word
+        self.n = n
+    def spamer(self, word, n):
+        i=0
+        for i in range(self.n):
+            sleep(1)
+            pg.typewrite(self.word)
+            pg.press("enter")
+
+
+ls = sys.argv[1:]
+print(ls)
+word = ls[0]
+n = ls[1]
+        
+spam1 = Spam("Hello",25)
+spam1.spamer(word, n)
+
+
+    
